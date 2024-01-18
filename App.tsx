95ff867255ab745a56946f887e7f185451d45938
ilker <ilker897@gmail.com>
2024-01-18 18:16:17 +0300

    fix: gesture handler

--- App.tsx
+++ App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import RootNavigation from './src/navigation/RootNavigation';
 import 'react-native-gesture-handler';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
 
 const App = () => {
-  return <RootNavigation />;
+  return (
+    <GestureHandlerRootView style={{flex: 1}}>
+      <RootNavigation />
+    </GestureHandlerRootView>
+  );
 };
 
 export default App;
